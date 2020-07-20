991e2658cbc79f1996049db249e08a3c29042a2e
Abdullah Ilgaz <mail@abdullahilgaz.com>
2020-07-20 07:03:32 +0300

    Update BooksForm.Designer.cs

--- Project/Views/BooksForm.Designer.cs
+++ Project/Views/BooksForm.Designer.cs
@@ -30,15 +30,16 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnNewBook = new System.Windows.Forms.Button();
-            this.txSearchBox = new System.Windows.Forms.TextBox();
-            this.btnSearch = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
             this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.authorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.genreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.pageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.btnNewBook = new System.Windows.Forms.Button();
+            this.txSearchBox = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
@@ -58,43 +59,6 @@
             this.dataGridView1.Size = new System.Drawing.Size(506, 453);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
-            // 
-            // btnNewBook
-            // 
-            this.btnNewBook.Location = new System.Drawing.Point(12, 481);
-            this.btnNewBook.Name = "btnNewBook";
-            this.btnNewBook.Size = new System.Drawing.Size(100, 45);
-            this.btnNewBook.TabIndex = 1;
-            this.btnNewBook.Text = "New Book";
-            this.btnNewBook.UseVisualStyleBackColor = true;
-            this.btnNewBook.Click += new System.EventHandler(this.btnNewBook_Click);
-            // 
-            // txSearchBox
-            // 
-            this.txSearchBox.Location = new System.Drawing.Point(244, 494);
-            this.txSearchBox.Name = "txSearchBox";
-            this.txSearchBox.Size = new System.Drawing.Size(181, 20);
-            this.txSearchBox.TabIndex = 2;
-            // 
-            // btnSearch
-            // 
-            this.btnSearch.Location = new System.Drawing.Point(431, 491);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(75, 23);
-            this.btnSearch.TabIndex = 3;
-            this.btnSearch.Text = "Search";
-            this.btnSearch.UseVisualStyleBackColor = true;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
-            // 
-            // btnDelete
-            // 
-            this.btnDelete.Location = new System.Drawing.Point(118, 481);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(100, 45);
-            this.btnDelete.TabIndex = 4;
-            this.btnDelete.Text = "Delete Book";
-            this.btnDelete.UseVisualStyleBackColor = true;
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // nameDataGridViewTextBoxColumn
             // 
@@ -124,11 +88,59 @@
             // 
             this.bindingSource1.DataSource = typeof(Project.Models.Book);
             // 
+            // btnNewBook
+            // 
+            this.btnNewBook.Location = new System.Drawing.Point(12, 500);
+            this.btnNewBook.Name = "btnNewBook";
+            this.btnNewBook.Size = new System.Drawing.Size(100, 45);
+            this.btnNewBook.TabIndex = 1;
+            this.btnNewBook.Text = "New Book";
+            this.btnNewBook.UseVisualStyleBackColor = true;
+            this.btnNewBook.Click += new System.EventHandler(this.btnNewBook_Click);
+            // 
+            // txSearchBox
+            // 
+            this.txSearchBox.Location = new System.Drawing.Point(12, 474);
+            this.txSearchBox.Name = "txSearchBox";
+            this.txSearchBox.Size = new System.Drawing.Size(432, 20);
+            this.txSearchBox.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(450, 468);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(68, 30);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(224, 500);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 45);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete Book";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(118, 500);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(100, 45);
+            this.btnView.TabIndex = 5;
+            this.btnView.Text = "View Book";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
             // BooksForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(530, 572);
+            this.ClientSize = new System.Drawing.Size(530, 555);
+            this.Controls.Add(this.btnView);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txSearchBox);
@@ -155,5 +167,6 @@
         private System.Windows.Forms.TextBox txSearchBox;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnView;
     }
 }
